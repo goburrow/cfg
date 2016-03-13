6133a224011bbee9c70a7e387f7f59479738b149
Quoc-Viet Nguyen <afelion@gmail.com>
2016-03-14 00:39:39 +1000

    Utilize append for slice copying

--- ini/walk.go
+++ ini/walk.go
@@ -25,12 +25,12 @@ func Walk(r io.Reader, walkFn WalkFunc) error {
 		}
 		if line[0] == '[' && line[len(line)-1] == ']' {
 			// Section
-			section = growAndCopy(section, bytes.TrimSpace(line[1:len(line)-1]))
+			section = duplicate(section, bytes.TrimSpace(line[1:len(line)-1]))
 			continue
 		}
 		if line[len(line)-1] == '\\' {
 			// Duplicate data from Scanner to append next lines
-			line = duplicate(line[:len(line)-1])
+			line = append([]byte(nil), line[:len(line)-1]...)
 
 			// Continuation lines
 			for scanner.Scan() {
@@ -61,31 +61,13 @@ func Walk(r io.Reader, walkFn WalkFunc) error {
 	return nil
 }
 
-// growAndCopy copies data from src to dest. It returns a new slice if dest capacity does not
+// duplicate copies data from src to dest. It returns a new slice if dest capacity does not
 // fit src length.
-func growAndCopy(dest, src []byte) []byte {
+func duplicate(dest, src []byte) []byte {
 	if cap(dest) < len(src) {
-		dest = makeSlice(len(src))
-	} else {
-		dest = dest[:len(src)]
+		return append([]byte(nil), src...)
 	}
-	copy(dest, src)
-	return dest
-}
-
-// makeSlice returns a slice that has length n. Its capacity is sligtly larger than needed.
-func makeSlice(n int) []byte {
-	// Grow to multiple of 256 (2^8)
-	const max = int(^uint(0x1ff) >> 1)
-	c := n
-	if c < max {
-		c = (c + 0xff) & ^0xff
-	}
-	return make([]byte, n, c)
-}
-
-func duplicate(src []byte) []byte {
-	dest := makeSlice(len(src))
+	dest = dest[:len(src)]
 	copy(dest, src)
 	return dest
 }
